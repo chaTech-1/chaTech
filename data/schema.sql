aa1b55e50856663e87301ea154912ef873a8a512
Aseel-Z <aseelalzweri@gmail.com>
2021-04-11 10:41:15 +0300

    end-points-v2-

--- data/schema.sql
+++ data/schema.sql
@@ -5,28 +5,28 @@ DROP TABLE IF EXISTS messages;
 
 
 CREATE TABLE admins(
-admin_id SERIAL PRIMARY KEY,
+adminid SERIAL PRIMARY KEY,
 name VARCHAR(225),
 password VARCHAR(225));
 
 
 CREATE TABLE rooms(
-room_id SERIAL PRIMARY KEY,
+roomid SERIAL PRIMARY KEY,
 name VARCHAR(225),
-admin_id FOREIGN KEY,
-REFERENCES admins(admin_id));
+adminid FOREIGN KEY REFERENCES admins(adminid));
 
 
 CREATE TABLE participants(
-participant_id SERIAL PRIMARY KEY,
-email VARCHAR(225),
-name VARCHAR(225));
+participantid SERIAL PRIMARY KEY,
+name VARCHAR(225),
+email VARCHAR(225)
+);
 
 
 CREATE TABLE messages(
-message_id INT PRIMARY KEY,
+messageid INT PRIMARY KEY,
 time DATETIME,
-message_body TEXT,
-room_id FOREIGN KEY,
-participant_id FOREIGN KEY,
-REFERENCES participants(participant_id) rooms(room_id));
+messagebody TEXT,
+roomid FOREIGN KEY REFERENCES rooms(roomid),
+participantid FOREIGN KEY REFERENCES participants(participantid)
+);
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const superagent = require('superagent')
 const pg = require('pg');
 const { response } = require('express');
 const methodoverride = require('method-override');
+// io
+// bycrypt
 
 // App Set-Up
 const app = express();
@@ -29,163 +31,184 @@ const DATABASE_URL = process.env.DATABASE_URL;
 const client = new pg.Client(DATABASE_URL);
 
 
+// endpoints
+app.get('/', renderhome);
+app.post('/',participantInfoHandler)
+app.get('/signin',renderSignin)
+app.post('/signin',handlerSignin);
+app.get('/dashboard',renderDashboard);
+app.post('/dashboard',addRoomToDashboard);
+app.put('/dashboard',editRoomInDashboard);
+app.delete('/dashboard',deleteRoomFromDashboard);
+app.get('/chatrooms',renderChatRoom);
 
-app.get('/', handlerhome);
-app.post('/userinfo',handleruserinfo);
-app.post('/sendmessage',handlersendmessage);
-app.get('/getmessage',handlerrecievemessage);
-app.post('/signin',handlersignin);
-app.post('/dashboard',handlerdashboard);
-app.post('/newchatroom',handlernewchatroom);
 
+// Call-Back Functions
 
-function handlerhome(request,response){
-
+function renderhome(request,response){
     response.render('../views/index')
-
 }
 
+//Sign-In - Admin
+function renderSignin(request,response) {
+    response.render('../views/admin/sign-in')
+}
 
+function handlerSignin(request,response){
+    // bycrypt - pacakge
+    // AUTHENTICATION REQUIRED
+    const{name,password}=request.body;
+    const sqlQuery =`INSERT INTO admins (name,password) VALUES ($1,$2);`
+    const safeValues= [name,password];
+                
+    client.query(sqlQuery,safeValues).then( function() {
+        response.redirect('/dashboard');
+    }).catch((error)=>{
+            errorHandler(error, response)
+        }) 
+        }
 
-//get the user data and insert it in the participants table
-function handleruserinfo(request,response){
+// Dashboard
 
-const{email,name}=request.body;
-const userQuery =`INSERT INTO participants (email,name) VALUES ($1,$2);`
-const userValues= [email,name];
+function renderDashboard(request,response) {
+    response.render('../views/admin/dashboard')
+}
 
-client.query(userQuery,userValues).then(data =>{
-    response.redirect('/chatroom');
-}).catch(error=>{
-    response.render('../views/error')
+// Add a chat room 
+function addRoomToDashboard(request,response) {
+    
+}
+// Edit a chat room
+function editRoomInDashboard(request,response) {
+    
+}
+// Delete a chat room 
+function deleteRoomFromDashboard(request,response) {
+    
+}
+
+// Chat Room 
+
+function renderChatRoom(request, response) {
+    response.render('../views/chatroom/chatroom')
+}
+
+// Participants
+
+function participantInfoHandler(request,response){
+const{name,email}=request.body;
+const sqlQuery =`INSERT INTO participants(name,email) VALUES($1,$2);`
+const safeValues= [name,email];
+// Rooms will be rendered 
+client.query(sqlQuery,safeValues).then(data =>{
+    response.redirect(`/chatrooms`);
+}).catch((error)=>{
+    errorHandler(error, response)
 })
 }
+
+// Messages 
+
+// Render Chat Thread
+
+// Post Message
+
+// Delete Message
+
+// Edit Message
+
+
+
+
+
+
+// -----------------------------------------------------------------------------------------------------------------------
+
+// app.post('/userinfo',handleruserinfo);
+// app.post('/sendmessage',handlersendmessage);
+// app.get('/getmessage',handlerrecievemessage);
+// app.post('/dashboard',handlerdashboard);
+// app.post('/newchatroom',handlernewchatroom);
+
+//get the user data and insert it in the participants table
+// function handleruserinfo(request,response){
+
+// const{email,name}=request.body;
+// const userQuery =`INSERT INTO participants(email,name) VALUES($1,$2);`
+// const userValues= [email,name];
+
+// client.query(userQuery,userValues).then(data =>{
+//     response.redirect('/chatroom');
+// }).catch(error=>{
+//     response.render('../views/error')
+// })
+// }
 
 
 
 // insert the message inside the messages table and redirect to the handler get messages 
-function handlersendmessage(request,response){
+// function handlersendmessage(request,response){
 
-    const{message_body}=request.body;
-    const userQuery =`INSERT INTO participants (message_body) VALUES ($1);`
-    const userValues= [message_body];
+//     const{message_body}=request.body;
+//     const userQuery =`INSERT INTO participants (message_body) VALUES ($1);`
+//     const userValues= [message_body];
     
-    client.query(userQuery,userValues).then(data =>{
-        response.redirect('/getmessage');
-    }).catch(error=>{
-        response.render('../views/error')
-    }) 
-    }
+//     client.query(userQuery,userValues).then(data =>{
+//         response.redirect('/getmessage');
+//     }).catch(error=>{
+//         response.render('../views/error')
+//     }) 
+//     }
 
 
 
  // get the messages from the messages table and render the message body in the chatroom.ejs (just need to make the template)
  //maybe we need to create an object ???
- function handlerrecievemessage(request,response){
+//  function handlerrecievemessage(request,response){
 
-     const{message_body}=request.body;
-        const userQuery =`SELECT time,message_body,participant_id FROM participants;`
-        const userValues= [message_body];
+//      const{message_body}=request.body;
+//         const userQuery =`SELECT time,message_body,participant_id FROM participants;`
+//         const userValues= [message_body];
         
-        client.query(userQuery,userValues).then(data =>{
-            response.render('./chatroom/chatroom',{data:data.rows[0]});
-        }).catch(error=>{
-            response.render('../views/error')
-        })
-        }
+//         client.query(userQuery,userValues).then(data =>{
+//             response.render('./chatroom/chatroom',{data:data.rows[0]});
+//         }).catch(error=>{
+//             response.render('../views/error')
+//         })
+//         }
 
 //render the dashboard after admins sign in 
-function handlerdashboard(request,response){
+// function handlerdashboard(request,response){
 
-    const{name}=request.body;
-     const userQuery =`SELECT name FROM rooms VALUES($);`
-     const userValues= [name];
+//     const{name}=request.body;
+//      const userQuery =`SELECT name FROM rooms VALUES($);`
+//      const userValues= [name];
                  
-     client.query(userQuery,userValues).then(data =>{
-         response.render('./chatroom/chatroom',{data:data.rows[0]});
-         }).catch(error=>{
-          response.render('../views/error')
-         }) 
-         }
-
-
-
-
-//post the admins data in the admins table 
-function handlersignin(request,response){
-
-    const{name,password}=request.body;
-    const userQuery =`INSERT INTO admins (name,password) VALUES ($1,$2);`
-    const userValues= [name,password];
-                
-    client.query(userQuery,userValues).then(data =>{
-        response.redirect('/dashboard');
-        }).catch(error=>{
-         response.render('../views/error')
-        }) 
-        }
-
-
-
-
-
-
-
-
+//      client.query(userQuery,userValues).then(data =>{
+//          response.render('./chatroom/chatroom',{data:data.rows[0]});
+//          }).catch(error=>{
+//           response.render('../views/error')
+//          }) 
+//          }
 //to add a new room but still not finish 
-function handlernewchatroom(request,response){
+// function handlernewchatroom(request,response){
 
-    const{name}=request.body;
-    const userQuery =`INSERT INTO rooms (name) VALUES ($1,$2);`
-    const userValues= [name];
+//     const{name}=request.body;
+//     const userQuery =`INSERT INTO rooms (name) VALUES ($1,$2);`
+//     const userValues= [name];
 
-    client.query(userQuery,userValues).then(data =>{
-        // response.render('');
-        }).catch(error=>{
-         response.render('../views/error')
-        }) 
+//     client.query(userQuery,userValues).then(data =>{
+//         // response.render('');
+//         }).catch(error=>{
+//          response.render('../views/error')
+//         }) 
     
-}
-
-
-
-
-
-// test routes with test handler functions
-// Routes - admin 
-// app.get('/home', home)
-// app.get('/chatroom', chatRoom)
-// app.get('/signIn', signIn)
-// app.get('/dashboard', dashBoard)
-
-// // /dashboard-signin - get
-// // /dashboard - put delete
-// // /chat/id
-// // /
-
-// // Handler
-// function home(request, response) {
-//     response.render('../views/index')
 // }
 
-// function chatRoom(request, response) {
-//     response.render('../views/chatroom/chatroom')
-// }
 
-// function signIn(request, response) {
-//     response.render('../views/admin/sign-in')
-// }
 
-// function dashBoard(request, response) {
-//     response.render('../views/admin/dashboard.ejs')
-// }
 
-// PORT Listen + Database connection
-
-// client.connect().then( function() {
-//     app.listen(PORT, () => console.log(`Listening to Port ${PORT}`))
-// });
-
+// -----------------------------------------------------------------------------------------------------------------------
 
 // All Errors
 
